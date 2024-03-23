6c0b554f451cc1a4581b693b56e58ffc93ae3209
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-23 14:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function volume_sphere() {
+// let value=document.getElementById("submit").addEventListerner("click",volume_sphere());
+function volume_sphere(event) {
+	event.preventDefault();
     //Write your code here
-  
+	let r=document.getElementById("radius").value;
+	let v=document.getElementById("volume");
+    // let subm=documet.getElementById("submit")
+	let ans= (4/3) * Math.PI * Math.pow(r, 3);
+	v.value=ans;
+	
 } 
+// let value=document.addEventListerner("click",volume_sphere());
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
